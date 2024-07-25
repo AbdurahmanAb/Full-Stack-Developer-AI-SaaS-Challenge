b16989e6646f2b017082f6eb6a40d5319cbfaabf
abdu <abdurahman>
2024-07-25 13:15:54 +0300

    day 2 implementing clerk and react drop zone

--- src/components/placeholderDocumnet.tsx
+++ src/components/placeholderDocumnet.tsx
@@ -1,10 +1,17 @@
+"use client"
+
 import React from 'react'
 import { Button } from './ui/button'
 import { PlusCircleIcon } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 export const PlaceHolderDocs = () => {
+    const router = useRouter()
+    const handleCLick = () => {
+        router.push('/dashboard/upload');
+    }
   return (
-    <Button className='flex flex-col items-center justify-center h-80 w-64 bg-gray-200 rounded-xl gap-3 hover:bg-slate-600 text-gray-400'>
+    <Button onClick={handleCLick} className='flex flex-col items-center justify-center h-80 w-64 bg-gray-200 rounded-xl gap-3 hover:bg-slate-600 text-gray-400'>
         <PlusCircleIcon size={60} />
         <p className='font-semibold '>Add A Document </p>
     </Button>
