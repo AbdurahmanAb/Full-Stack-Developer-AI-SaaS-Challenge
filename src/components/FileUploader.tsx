36efeba468c80c8784d095237da27f0fcc00e8f3
abdu <abdurahman>
2024-07-25 13:25:58 +0300

    day 2 implementing clerk and react drop zone

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -12,7 +12,7 @@ const FileUploader = () => {
       {
         isDragActive ?
           <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
+          <p>Drag  drop some files here, or click to select files</p>
       }
     </div>
   )
