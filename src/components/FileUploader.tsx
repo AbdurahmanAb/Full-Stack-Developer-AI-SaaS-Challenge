8cba301b33f84ba0183167d25a327c02878d8eda
abdu <abdurahman>
2024-07-25 13:38:41 +0300

    day 2 implementing clerk and react drop zone

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -4,10 +4,12 @@ import { useDropzone } from "react-dropzone";
 const FileUploader = () => {
   const onDrop = useCallback((acceptedFiles:File[]) => {
     // Do something with the files
+    console.log(acceptedFiles);
+    
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
   return (
-    <div {...getRootProps()}>
+    <div {...getRootProps()} className="p-8 border-indigo-600 text-indigo-600 border-2 border-dashed rounded-lg h-96 flex items-center">
       <input {...getInputProps()} />
       {
         isDragActive ?
